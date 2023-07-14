dbbe7bc5b576ca963c7ce408380ffd3be4d65776
LoilaLeila <addisonloislyn@gmail.com>
2023-07-13 18:41:00 -0700

    PUTS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description: A program that prints a line with puts function
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle.");
+	return (0);
+}
