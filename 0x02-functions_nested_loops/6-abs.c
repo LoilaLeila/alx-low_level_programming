ac74a26dbd79aa663f26bb47c04e0f442130da60
LoilaLeila <addisonloislyn@gmail.com>
2023-07-19 00:18:41 -0700

    AbsoluteNo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - shows the absolute number of an integer
+ * @i: parameter
+ * Return: i and -i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	else
+	{
+		return (i);
+	}
+	return (0);
+}
