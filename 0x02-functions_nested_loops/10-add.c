efa4b60024f8b671a50caf01d218700ac587ef93
LoilaLeila <addisonloislyn@gmail.com>
2023-07-19 09:29:41 -0700

    ADD2

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - a program that adds two integers
+ * @a: parameter
+ * @b: parameter
+ * Return: (0) (Success)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+		_putchar(c + '0');
+	return (c);
+}
