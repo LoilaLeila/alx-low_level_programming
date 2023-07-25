4b1061341d3d045f4ed65d39ee67cbcd34d5fcf0
LoilaLeila <addisonloislyn@gmail.com>
2023-07-24 17:24:15 -0700

    String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - finds the length of a string
+* @s - parameter for string
+* Return: (0) (Success)
+*/
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
