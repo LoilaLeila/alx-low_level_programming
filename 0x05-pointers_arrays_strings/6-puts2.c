ed23ccd2b877ce20cc8abd81fc465b967d38360c
LoilaLeila <addisonloislyn@gmail.com>
2023-07-24 19:35:21 -0700

    PUTS 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* puts2 - prints every other line of  a string
+* @str: string parameter
+* Return: (0)
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
