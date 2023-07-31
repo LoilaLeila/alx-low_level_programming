c53d2626a33ba68de5d08af7aeebbc6dbe38cceb
LoilaLeila <addisonloislyn@gmail.com>
2023-07-31 16:31:22 -0700

    String Character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _strchr - a function that locates a character in a string
+* @s: a pointer parameter
+* @c: character parameter
+* Return: s or NULL
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+			s++;
+		}
+	}
+	if (*s == c)
+		return (s);
+	return (NULL);
+}
