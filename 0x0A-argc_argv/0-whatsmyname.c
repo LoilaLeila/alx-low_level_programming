131421c44b9eceae5dac21d458f7361f681d778a
LoilaLeila <addisonloislyn@gmail.com>
2023-08-03 18:05:35 -0700

    Prints Name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,13 +9,9 @@ int main(int argc, char *argv[])
 {
 	if (argc >= 1)
 	{
-		printf("Program name is: %s\n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
-	else
-	{
-		printf("Unable to get program name\n");
-	}
-	return (0);
+		return (0);
 }
 
 
