86ba09693964d83bbb5b60f6ab53d24cc05e68d2
LoilaLeila <addisonloislyn@gmail.com>
2023-08-09 13:42:34 -0700

    Concatenate Arguement

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -16,7 +16,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 		totallength += strlen(av[i]) + 1;
-	result = malloc(sizeof(char) * totallength);
+	result = malloc(sizeof(char) * totallength + 1);
 
 	if (result == NULL)
 		return (NULL);
