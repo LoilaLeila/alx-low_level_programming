2705f18e5b099cf692f663226d5c590fd3e5da49
LoilaLeila <addisonloislyn@gmail.com>
2023-08-09 13:11:11 -0700

    Concatenate Arguement

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+* argstostr - concatenates all arguments
+* @ac: parameter
+* @av: parameter
+* Return: Null, or pointer
+*/
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int totallength = 0;
+	char *result;
+	int position = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		totallength += strlen(av[i]) + 1;
+	result = malloc(sizeof(char) * totallength);
+
+	if (result == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(result + position, av[i]);
+		position += strlen(av[i]);
+		result[position] = '\n';
+		position++;
+	}
+	result[position - 1] = '\0';
+	return (result);
+}
