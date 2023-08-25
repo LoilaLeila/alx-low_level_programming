6c987954ef95051f520e3c478f5a408e10622ba1
LoilaLeila <addisonloislyn@gmail.com>
2023-08-24 20:47:00 -0700

    Free List

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
